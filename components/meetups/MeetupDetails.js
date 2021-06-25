a7eac6fcb50a35cd9ff830fdbd65a84e7135ad55
debaosuidecl <36285179+debaosuidecl@users.noreply.github.com>
2021-06-25 05:44:37 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ components/meetups/MeetupDetails.js
@@ -0,0 +1,19 @@
+import classes from "./MeetupDetails.module.css"
+import Image from "next/image"
+
+function MeetupDetails(props) {
+    return (
+        <section className={classes.detail}>
+        <img layout='fill' src={props.image}
+alt={props.title}/>
+
+
+    <h1>{props.title}</h1>
+<address>{props.address}</address>
+    <p>{props.description}</p>
+    </section>
+
+    )
+}
+
+export default MeetupDetails
